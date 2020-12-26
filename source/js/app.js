90ea259a184652784c0970b738970f808b0ec7c4
codedayang <codedayang@outlook.com>
2020-12-26 22:53:21 +0800

    样式修改

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,11 @@
 ;(function () {
-  document.body.insertAdjacentHTML("beforeend", "<div id=\"loading\"><i class=\"load-info\"></i></div>");
-  document.addEventListener("readystatechange", function () {
-    if (document.readyState === "complete") {
-      var $load = document.getElementById("loading");
-      document.body.removeChild($load);
-    }
-  });
+  // document.body.insertAdjacentHTML("beforeend", "<div id=\"loading\"><i class=\"load-info\"></i></div>");
+  // document.addEventListener("readystatechange", function () {
+  //   if (document.readyState === "complete") {
+  //     var $load = document.getElementById("loading");
+  //     document.body.removeChild($load);
+  //   }
+  // });
 })();
 
 window.onload = function () {
